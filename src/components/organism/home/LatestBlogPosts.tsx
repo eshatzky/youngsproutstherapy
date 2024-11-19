1427deec18e61d799d09546e9861050f69c76586
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-11-19 18:07:05 +0600

    Added thank you page

--- src/components/organism/home/LatestBlogPosts.tsx
+++ src/components/organism/home/LatestBlogPosts.tsx
@@ -4,7 +4,7 @@ import { getLatestThreePosts } from "@/lib/client";
 export async function LatestBlogPosts() {
   const posts = await getLatestThreePosts();
   return (
-    <section className="max-w-[1440px] mx-auto w-full ~px-5/10 py-8 ~pb-20/24 border-[#8A969E] flex flex-col ~gap-7/16 @container/latestBlogs ">
+    <section className="max-w-[1440px] mx-auto w-full ~px-5/10 pt-16 ~pb-20/24 border-[#8A969E] flex flex-col ~gap-7/16 @container/latestBlogs ">
       <h2 className="text-3xl font-semibold text-center">
         Latest <span className="text-primary">Blog Posts</span>
       </h2>
