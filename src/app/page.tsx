dbfa79f0bfb2691a76f396d03b70d533a53bba41
Eric Shatzky <106122967+eshatzky@users.noreply.github.com>
2025-01-28 06:06:50 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
 }
 
 export const metadata = {
-  title: "Young Sprouts | Child, Teen &amp; Family Therapy | Vaughan",
+  title: "Young Sprouts | Child, Teen and Family Therapy | Vaughan",
   description:
     "Psychotherapy and counseling for kids &amp; teens in Vaughan. Find support for kids &amp; teens struggling with anxiety, behavioral issues, ADHD, grief, trauma, and more.",
 };
