e381b25ab381bd9c6afb48751191ba89f246c070
Eric Shatzky <106122967+eshatzky@users.noreply.github.com>
2025-01-28 15:17:49 -0500

    Update index.tsx

--- src/components/template/site/location/index.tsx
+++ src/components/template/site/location/index.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
 const locations = {
-  thornhill: {
+  Thornhill: {
     name: "Thornhill",
     description:
       "Expert tutoring services in the Thornhill area for elementary and high school students.",
@@ -11,7 +11,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.123456789012!2d-79.46416348248638!3d43.807686617812735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2c2f7a900001%3A0xa6b26c04612c3e36!2sThornhill%20Woods%20Dr%2C%20Thornhill%2C%20ON!5e0!3m2!1sen!2sca!4v1731796678561!5m2!1sen!2sca",
   },
-  vaughan: {
+  Vaughan: {
     name: "Vaughan",
     description:
       "Expert tutoring services in the Vaughan area for elementary and high school students.",
@@ -19,7 +19,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92092.7878805373!2d-79.64834130533858!3d43.83717922378304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b268883243a77%3A0xdd95317585aa054b!2sVaughan%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738062693891!5m2!1sen!2sbd",
   },
-  "richmond-hill": {
+  "Richmond Hill": {
     name: "Richmond Hill",
     description:
       "Expert tutoring services in the Richmond Hill area for elementary and high school students.",
@@ -27,7 +27,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91990.11678028788!2d-79.51070175245174!3d43.90366303302156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2a4936291733%3A0x5201760ad6b142ed!2sRichmond%20Hill%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056591650!5m2!1sen!2sbd",
   },
-  markham: {
+  Markham: {
     name: "Markham",
     description:
       "Expert tutoring services in the Markham area for elementary and high school students.",
@@ -35,7 +35,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92025.50477592275!2d-79.45542543996659!3d43.880756894266824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d5efa0324ca9%3A0xf73d52812cb23d63!2sMarkham%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056624450!5m2!1sen!2sbd",
   },
-  "north-york": {
+  "North York": {
     name: "North York",
     description:
       "Expert tutoring services in the North York area for elementary and high school students.",
@@ -43,7 +43,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92220.67808331583!2d-79.52439880893407!3d43.75425244934779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b322ec4918c75%3A0xfb330d877abf7e3d!2sNorth%20York%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056661066!5m2!1sen!2sbd",
   },
-  maple: {
+  Maple: {
     name: "Maple",
     description:
       "Expert tutoring services in the Maple area for elementary and high school students.",
@@ -51,7 +51,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23017.88698775727!2d-79.54574984354883!3d43.850939548387714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b29035d80589d%3A0xe9c39e17ba1ddefd!2sMaple%2C%20Vaughan%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056689225!5m2!1sen!2sbd",
   },
-  concord: {
+  Concord: {
     name: "Concord",
     description:
       "Expert tutoring services in the Concord area for elementary and high school students.",
@@ -59,7 +59,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46069.17389504498!2d-79.54892129341908!3d43.80765154245235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2ef69e4bab3b%3A0x8f3aa5e88bdaf18c!2sConcord%2C%20Vaughan%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056720496!5m2!1sen!2sbd",
   },
-  woodbridge: {
+  Woodbridge: {
     name: "Woodbridge",
     description:
       "Expert tutoring services in the Woodbridge area for elementary and high school students.",
@@ -67,7 +67,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46072.986535262666!2d-79.62553654347175!3d43.802707997632005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b257a9ad0b911%3A0xecea3dfb3abd0ac2!2sWoodbridge%2C%20Vaughan%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056750073!5m2!1sen!2sbd",
   },
-  aurora: {
+  Aurora: {
     name: "Aurora",
     description:
       "Expert tutoring services in the Aurora area for elementary and high school students.",
@@ -75,7 +75,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45923.45527782772!2d-79.4876203914055!3d43.996261394655534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ad392943890c7%3A0x5037b28c7231a30!2sAurora%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056773425!5m2!1sen!2sbd",
   },
-  newmarket: {
+  Newmarket: {
     name: "Newmarket",
     description:
       "Expert tutoring services in the Newmarket area for elementary and high school students.",
@@ -83,7 +83,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45881.585931278314!2d-79.50240539082675!3d44.050335787882894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ad20bf0ae014f%3A0xcb39bf730072dc8f!2sNewmarket%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056796017!5m2!1sen!2sbd",
   },
-  mississauga: {
+  Mississauga: {
     name: "Mississauga",
     description:
       "Expert tutoring services in the Mississauga area for elementary and high school students.",
@@ -91,7 +91,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184985.37627873477!2d-79.6591540468505!3d43.577456818359416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b469fe76b05b7%3A0x3146cbed75966db!2sMississauga%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056819617!5m2!1sen!2sbd",
   },
-  brampton: {
+  Brampton: {
     name: "Brampton",
     description:
       "Expert tutoring services in the Brampton area for elementary and high school students.",
@@ -99,7 +99,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184531.2943256528!2d-79.92435017044492!3d43.725063729050355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b15eaa5d05abf%3A0x352d31667cc38677!2sBrampton%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056841809!5m2!1sen!2sbd",
   },
-  pickering: {
+  Pickering: {
     name: "Pickering",
     description:
       "Expert tutoring services in the Pickering area for elementary and high school students.",
@@ -107,7 +107,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d368811.97221155814!2d-79.4060868189731!3d43.76572726918989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d9abfae88d1d%3A0x4bd24862b12786d8!2sPickering%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056866120!5m2!1sen!2sbd",
   },
-  ajax: {
+  Ajax: {
     name: "Ajax",
     description:
       "Expert tutoring services in the Ajax area for elementary and high school students.",
@@ -115,7 +115,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46021.905634133705!2d-79.06996874276604!3d43.86890362825505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4e7b441ad15d1%3A0xfb77e101d380bece!2sAjax%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056893201!5m2!1sen!2sbd",
   },
-  whitby: {
+  Whitby: {
     name: "Whitby",
     description:
       "Expert tutoring services in the Whitby area for elementary and high school students.",
@@ -123,7 +123,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d368609.0764671951!2d-79.24232619380012!3d43.79862587324898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51e88fc357749%3A0xd77b28711c4012cb!2sWhitby%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056921480!5m2!1sen!2sbd",
   },
-  oshawa: {
+  Oshawa: {
     name: "Oshawa",
     description:
       "Expert tutoring services in the Oshawa area for elementary and high school students.",
@@ -131,7 +131,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91920.8809585992!2d-78.96075135050472!3d43.94845088819139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51d21ccd37533%3A0xdd8ceff2f844fcbf!2sOshawa%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056943172!5m2!1sen!2sbd",
   },
-  barrie: {
+  Barrie: {
     name: "Barrie",
     description:
       "Expert tutoring services in the Barrie area for elementary and high school students.",
@@ -139,7 +139,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91283.25504687845!2d-79.74812233256644!3d44.359232368296574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882aa3371ad7206d%3A0x1d2e0f00a84b535a!2sBarrie%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056965150!5m2!1sen!2sbd",
   },
-  hamilton: {
+  Hamilton: {
     name: "Hamilton",
     description:
       "Expert tutoring services in the Hamilton area for elementary and high school students.",
@@ -147,7 +147,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185954.92503790412!2d-80.09795545321366!3d43.26094214425763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882c986c27de778f%3A0x2b6aee56d8df0e21!2sHamilton%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738056990968!5m2!1sen!2sbd",
   },
-  guelph: {
+  Guelph: {
     name: "Guelph",
     description:
       "Expert tutoring services in the Guelph area for elementary and high school students.",
@@ -155,7 +155,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92558.6039758035!2d-80.32256836843158!3d43.53452786447026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b9ad0c3a9fb6b%3A0x5037b28c7231b60!2sGuelph%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738057016482!5m2!1sen!2sbd",
   },
-  kitchener: {
+  Kitchener: {
     name: "Kitchener",
     description:
       "Expert tutoring services in the Kitchener area for elementary and high school students.",
@@ -163,7 +163,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92718.07427072432!2d-80.55871542291221!3d43.43052943458779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf48c03ee5105%3A0x9525f8e6df5f544b!2sKitchener%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738057040332!5m2!1sen!2sbd",
   },
-  waterloo: {
+  Waterloo: {
     name: "Waterloo",
     description:
       "Expert tutoring services in the Waterloo area for elementary and high school students.",
@@ -171,7 +171,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46319.38434870205!2d-80.5879188468746!3d43.48227338285425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf1565ffe672b%3A0x5037b28c7231d90!2sWaterloo%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738057058616!5m2!1sen!2sbd",
   },
-  cambridge: {
+  Cambridge: {
     name: "Cambridge",
     description:
       "Expert tutoring services in the Cambridge area for elementary and high school students.",
@@ -179,7 +179,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39127.18402945437!2d0.07878780331512417!3d52.19888753470316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d85d89f32a012d%3A0x63a320e1a35e3d21!2sCambridge%2C%20UK!5e0!3m2!1sen!2sbd!4v1738057081584!5m2!1sen!2sbd",
   },
-  "niagara-falls": {
+  "Niagara Falls": {
     name: "Niagara Falls",
     description:
       "Expert tutoring services in the Niagara Falls area for elementary and high school students.",
@@ -187,7 +187,7 @@ const locations = {
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93292.95510977176!2d-79.24392174371238!3d43.053952162085544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d3445eec824db9%3A0x46d2c56156bda288!2sNiagara%20Falls%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1738057114343!5m2!1sen!2sbd",
   },
-  "st.-catharines": {
+  "St. Catherines": {
     name: "St. Catharines",
     description:
       "Expert tutoring services in the St. Catharines area for elementary and high school students.",
